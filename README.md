c54af2d09d6ee4beb331de431c0df4c2f3d44a7e
Raphael Odini <raphael.odini@gmail.com>
2018-12-03 10:05:45 +0800

    add files. add script to enrich with geoname. improve readme

--- README.md
+++ README.md
@@ -1,19 +1,59 @@
 # CSV & TopoJSON data for Cartography usage
 
-## Without Antarctica ?
+## Without Antarctica?
 
 removed Polygons with id's:
 - 010 (Antarctica)
 - 260 (French Southern Territories)
 
-## Enriched ?
+## Enriched?
 
-Each Polygon is defined by an `id`. I added 5 extra fields:
-- `country_name_en`
-- `country_code_2`
-- `country_code_3`
-- `country_region_en`
-- `country_sub_region_en`
+Each Polygon is defined by an `id`. I added a couple of extra fields.
+
+filename: `..._enriched.json`
+```
+country_name_en
+country_code_2
+country_code_3
+country_region_en
+country_sub_region_en
+```
+
+filename `..._enriched_with_capital.json`
+```
+country_name_en
+country_code_2
+country_code_3
+country_region_en
+country_sub_region_en
+country_capital_en
+country_capital_lat
+country_capital_lng
+```
+
+filename `..._enriched_with_capital_and_geoname.json`
+```
+country_name_en
+country_code_2
+country_code_3
+country_region_en
+country_sub_region_en
+country_capital_en
+country_capital_lat
+country_capital_lng
+geoname_id
+```
+
+filename `..._enriched_with_geoname.json`
+```
+country_name_en
+country_code_2
+country_code_3
+country_region_en
+country_sub_region_en
+geoname_id
+```
+
 
 ### Example
 
@@ -25,6 +65,10 @@ Each Polygon is defined by an `id`. I added 5 extra fields:
   "country_code_3": "FRA",
   "country_region_en": "Europe",
   "country_sub_region_en": "Western Europe",
+  "country_capital_en": "Paris",
+  "country_capital_lat": 48.85341,
+  "country_capital_lng": 2.3488,
+  "geoname_id": 3017382,
   "type": "MultiPolygon",
   "arcs": [...]
 }
--- topojson/process_topojson.py
+++ topojson/process_topojson.py
@@ -1,8 +1,3 @@
-# coding='utf-8'
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
-
 # Import the modules
 import json
 import csv
@@ -17,12 +12,13 @@ def enrich_topojson(topojson_filename):
     countries = pd.read_csv("../countries_capitals/country_alphas_regions_continent_enriched_capitals.csv", index_col="country-code", encoding="utf-8", delimiter=",", keep_default_na=False, na_values=[''])
     countries = countries.fillna("")
 
-    print countries.shape
-
+    print(countries.shape)
 
     for idx, geometry in enumerate(topojson_data["objects"]["countries"]["geometries"]):
         if topojson_data["objects"]["countries"]["geometries"][idx]["id"] >= 0:
+            # find country row in csv
             row = countries.loc[topojson_data["objects"]["countries"]["geometries"][idx]["id"]]
+            # create properties object
             topojson_data["objects"]["countries"]["geometries"][idx]["properties"] = {}
             topojson_data["objects"]["countries"]["geometries"][idx]["properties"]["country_code_2"] = row["alpha-2"]
             topojson_data["objects"]["countries"]["geometries"][idx]["properties"]["country_code_3"] = row["alpha-3"]
@@ -30,21 +26,76 @@ def enrich_topojson(topojson_filename):
             topojson_data["objects"]["countries"]["geometries"][idx]["properties"]["country_pretty_name_en"] = row["pretty-name"]
             topojson_data["objects"]["countries"]["geometries"][idx]["properties"]["country_region_en"] = row["region"]
             topojson_data["objects"]["countries"]["geometries"][idx]["properties"]["country_sub_region_en"] = row["sub-region"]
+            # topojson_data["objects"]["countries"]["geometries"][idx]["properties"]["country_capital_en"] = row["capital-name"]
+            # topojson_data["objects"]["countries"]["geometries"][idx]["properties"]["country_capital_lat"] = row["capital-lat"]
+            # topojson_data["objects"]["countries"]["geometries"][idx]["properties"]["country_capital_lng"] = row["capital-lng"]
+            print(topojson_data["objects"]["countries"]["geometries"][idx])
+
+
+    with open(topojson_filename + "_enriched.json", "w") as f:
+        print('print to file')
+        json.dump(topojson_data, f)
+        # f.write(json.dumps(topojson_data))
+
+
+def add_capital(topojson_filename):
+    
+    with open(topojson_filename + ".json") as data_file:
+        topojson_data = json.load(data_file)
+    
+    countries = pd.read_csv("../countries_capitals/country_alphas_regions_continent_enriched_capitals.csv", index_col="country-code", encoding="utf-8", delimiter=",", keep_default_na=False, na_values=[''])
+    countries = countries.fillna("")
+
+    print(countries.shape)
+
+    for idx, geometry in enumerate(topojson_data["objects"]["countries"]["geometries"]):
+        print(idx, topojson_data["objects"]["countries"]["geometries"][idx]["id"])
+        if topojson_data["objects"]["countries"]["geometries"][idx]["id"] >= 0:
+            # find country row in capitals csv
+            row = countries.loc[topojson_data["objects"]["countries"]["geometries"][idx]["id"]]
+            # enrich properties object
             topojson_data["objects"]["countries"]["geometries"][idx]["properties"]["country_capital_en"] = row["capital-name"]
             topojson_data["objects"]["countries"]["geometries"][idx]["properties"]["country_capital_lat"] = row["capital-lat"]
             topojson_data["objects"]["countries"]["geometries"][idx]["properties"]["country_capital_lng"] = row["capital-lng"]
-            print topojson_data["objects"]["countries"]["geometries"][idx]
+            print(topojson_data["objects"]["countries"]["geometries"][idx])
 
 
-    with open(topojson_filename + "_enriched_with_capital.json", "w") as f:
+    with open(topojson_filename + "_with_capital.json", "w") as f:
+        print('print to file')
         json.dump(topojson_data, f)
-        # f.write(json.dumps(topojson_data))
+
+
+
+def add_geonameid(topojson_filename):
+
+    with open(topojson_filename + ".json") as data_file:
+        topojson_data = json.load(data_file)
+    
+    geonames = pd.read_csv("../countries_capitals/geonames.csv", index_col="ISO-Numeric", encoding="utf-8", delimiter=",", keep_default_na=False, na_values=[''])
+    geonames = geonames.fillna("")
+
+    print(geonames.shape)
+
+    for idx, geometry in enumerate(topojson_data["objects"]["countries"]["geometries"]):
+        if topojson_data["objects"]["countries"]["geometries"][idx]["id"] >= 0:
+            # find country row in geonames csv
+            row = geonames.loc[topojson_data["objects"]["countries"]["geometries"][idx]["id"]]
+            # enrich properties object
+            topojson_data["objects"]["countries"]["geometries"][idx]["properties"]["geoname_id"] = int(row["geonameid"])
+            print(topojson_data["objects"]["countries"]["geometries"][idx])
+
+    with open(topojson_filename + "_with_geoname.json", "w") as f:
+        print('print to file')
+        json.dump(topojson_data, f)
+
 
 """
 main()
 """
 if __name__ == '__main__':
 
-    topojson_filename = "world-50m-without-antarctica"
+    topojson_filename = "world-50m-without-antarctica_enriched_with_capital"
 
-    enrich_topojson(topojson_filename)
+    # enrich_topojson(topojson_filename)
+    # add_capital(topojson_filename)
+    # add_geonameid(topojson_filename)
